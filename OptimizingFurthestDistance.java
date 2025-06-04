4d8d426220e1bca634e4c8520cbe82bc131e6072
Leslie2101 <leslienguyen2101@gmail.com>
2025-06-04 18:02:25 +0930

    find furthest distance between 2 non-equal numbers

--- OptimizingFurthestDistance.java
+++ OptimizingFurthestDistance.java
@@ -1,14 +1,28 @@
 public class OptimizingFurthestDistance {
     public int solution(int[] A){
-        // find furthest distance between 2 non-equal numbers 
+        // Time complexity: O(N)
+        // Space complexity: O(1)
 
+        // find furthest distance between 2 non-equal numbers 
         int N = A.length;
+
+
+        // first possible value: A[0] with A[i] where i is as high as possible
         int result = 0;
+
+        for (int i = N-1; i >= 0; i--){
+            if (A[i] != A[0]){
+                result = Math.max(i+1, result);
+                break;
+            }
+        }
+
+
+        // second possible case: A[N-1] with A[i] where i is as low as possible
         for (int i = 0; i < N; i++){
-            for (int j = i; j < N; j++){
-                if (A[i] != A[j]){
-                    result = Math.max(result, j-i);
-                }
+            if (A[i] != A[N-1]){
+                result = Math.max(N-i, result);
+                break;
             }
         }
 
