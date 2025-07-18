6c05a6b50a2c18dfd1ba4cab931914fb94ed42da
Leslie2101 <leslienguyen2101@gmail.com>
2025-07-18 23:09:27 +0930

    ticket change

--- PublicTransportTicket.java
+++ PublicTransportTicket.java
@@ -1,9 +1,30 @@
+import java.util.Arrays;
+
 public class PublicTransportTicket {
     public static int solution(int[] A){
 
-        int minimumCost = 25;
 
-        return minimumCost;
+        int[] dp = new int[A.length+1];
+
+
+        for (int i = 1; i < A.length + 1; i++){
+            // valid from buying ticket 1-day 
+            dp[i] = dp[i-1] + 2;
+
+            // valid from buying ticket 7-day previously (note that A[j] is the last valid day)
+            for (int j = i-1; j == 0 || (j-1 >= 0 && A[i-1] - A[j-1] <= 7); j--){
+                dp[i] = Math.min(dp[i], dp[j] + 7);
+            }
+
+
+            // System.out.println("i " + i +  " days " + A[i-1] + ", " + dp[i]);
+
+        }
+
+        // System.out.println(Arrays.toString(dp));
+
+
+        return Math.min(dp[A.length], 25);
     }
 
 
